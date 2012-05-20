bf311dc5bcc9c64fedcbc34fad698712b18931ab
Mikael Kauppinen <mikael.kauppinen@iki.fi>
2012-05-20 15:28:01 +0300

    Fifth commit, fixed the compiler error by replacing FNV-constants with their hexadecimal representation.

--- awesome-hash.c
+++ awesome-hash.c
@@ -7,8 +7,8 @@
 uint64_t hashGenerate(const char* input, size_t length) {
 	assert(input);
 	
-	uint64_t FNV_Offset_Basis = 14695981039346656037;
-	uint64_t FNV_Prime = 1099511628211;
+	uint64_t FNV_Offset_Basis = 0x14650FB0739D0383;
+	uint64_t FNV_Prime = 0x100000001B3;
 
 	uint64_t hash = FNV_Offset_Basis; // Well apparently hash isn't a reserved word in C
 
