e9aa12feb2d99090b6b7fd6eac60393893003868
Mikael Kauppinen <mikael.kauppinen@iki.fi>
2012-05-20 23:11:39 +0300

    n:th commit. Major fixes on used offset base values, more to the subject in comments of the awesome-hash.c.
    Sample main program made more 'intuitive'.
    
    README updated to be more useful than just a plain greeting.

--- awesome-hash.c
+++ awesome-hash.c
@@ -7,10 +7,37 @@
 uint64_t hashGenerate(const char* input, size_t length) {
 	assert(input);
 	
-	uint64_t FNV_Offset_Basis = 0xCBF29CE484222000;
+	/*
+	 * Having some major troubles converting the FNV offset basis into
+	 * hexadecimal form. So here, for all to be clear, the correct number
+	 * in basis 10:
+	 * 
+	 * 14695981039346656037
+	 * 
+	 * Ubuntu converted this to base 16 as following:
+	 * 
+	 * 0xCBF29CE484222325
+	 * 
+	 * Then, again, various online resources gave me the value
+	 * 
+	 * 0xCBF29CE484222000
+	 * 
+	 * And Windows value was something completely different.
+	 * 
+	 * So, which one of these is correct? At this point, I decide to 
+	 * trust Ubuntu on this.
+	 * 
+	 */
+	
+	uint64_t FNV_Offset_Basis = 0xCBF29CE484222325;
 	uint64_t FNV_Prime = 0x100000001B3;
-
-	uint64_t hash = FNV_Offset_Basis; // Well apparently hash isn't a reserved word in C
+	
+	
+	/* 
+	 * Well apparently hash isn't a reserved word in C.
+	 */
+	
+	uint64_t hash = FNV_Offset_Basis;
 
 	for ( size_t i = 0; i < length; i++ ) {
 		hash = hash * FNV_Prime;
--- main.c
+++ main.c
@@ -5,13 +5,15 @@
 
 int main(void) {
 	
+	printf("Plain English keywords and their FNV hash representations, in base 16.\n");
+	
 	const char *jadajada = "Helsinki, Finland";
 	const char *jadajada2 = "KauppinenMikael";
 	const char *jadajada3 = "Kauppinen, Mikael";
 	
-	printf("%llu\n", hashGenerate(jadajada, 17));
-	printf("%llu\n", hashGenerate(jadajada2, strlen(jadajada2)));
-	printf("%llu\n", hashGenerate(jadajada3, strlen(jadajada3)));
+	printf("%s:\t%llX\n", jadajada, hashGenerate(jadajada, 17));
+	printf("%s:\t%llX\n", jadajada2, hashGenerate(jadajada2, strlen(jadajada2)));
+	printf("%s:\t%llX\n", jadajada3, hashGenerate(jadajada3, strlen(jadajada3)));
 	
 	return 0;
 }
