bb433230e03b4ea6fd32681def25df46b034f609
Mikael Kauppinen <mikael.kauppinen@iki.fi>
2012-05-20 15:38:00 +0300

    Sixth commit... Apparently the first FNV Prime in hexadecimal was wrong.

--- awesome-hash.c
+++ awesome-hash.c
@@ -7,7 +7,7 @@
 uint64_t hashGenerate(const char* input, size_t length) {
 	assert(input);
 	
-	uint64_t FNV_Offset_Basis = 0x14650FB0739D0383;
+	uint64_t FNV_Offset_Basis = 0xCBF29CE484222000;
 	uint64_t FNV_Prime = 0x100000001B3;
 
 	uint64_t hash = FNV_Offset_Basis; // Well apparently hash isn't a reserved word in C
